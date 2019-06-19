8a81c7250d63e26cd58ad2e2d64afe20292200ac
Boogerwooger <boogerwooger@mail.ru>
2019-06-19 16:16:55 +0000

    fixes and subscription to balance change

--- src/steps/PolkadotModuleBenchStep.ts
+++ src/steps/PolkadotModuleBenchStep.ts
@@ -52,59 +52,59 @@ export default class PolkadotModuleBenchStep extends BenchStep {
         let intt = parseInt(seed.replace(/[^0-9]/g, ''), 10);
 		intt++;
 		intt = intt % 1000;
-        return this.current_seed_base + ("000" + intt).slice(-4);
+        return this.current_seed_base + ("00" + intt).slice(-3);
     }
-    next_receiver_seed(seed: string) {
-        // to generate 999 seeds, constructed from sender's seed
-        // for 0045800 returns 00458001
-        // for 00458042 returns 00458043
-        // for 00458999 returns 00458000 (oveflows at 999 to repeat sender every 1000 tx)
-        //
-        let intt = parseInt(seed.replace(/[^0-9]/g, ''), 10);
-        if (intt < 1000) {
-            intt *= 100;
+    
+    getRandomReceiverSeed(senderSeed: string) {
+        let seed: string = this.current_seed_base + ("000" + Math.floor(Math.random() * 1000)).slice(-4);
+        while (seed == senderSeed) {
+            seed = this.current_seed_base + ("000" + Math.floor(Math.random() * 1000)).slice(-4);
         }
-        intt = intt % 10000;
-		intt++;
-        return this.current_seed_base + ("00000" + intt).slice(-6);
+        return seed;
     }
-
 
     async commitBenchmarkTransaction(uniqueData: any) {
+        // 
         this.current_amount = Math.floor(this.current_amount / 2);
-        
-        this.current_seed = this.next_receiver_seed(this.current_seed);
 
         // switch to next sender after all funds of current_sender finished
         if (this.current_amount <= 1) {
             this.current_sender_seed = await this.next_sender_seed(this.current_sender_seed);
             this.current_sender = await this.keyring.addFromUri(this.current_sender_seed);
             this.current_sender_nonce = await this.api.query.system.accountNonce(this.current_sender.address());
-            this.current_seed = await this.current_sender_seed + "00";
+            this.current_seed  = this.getRandomReceiverSeed(this.current_sender_seed);
 			this.current_amount = await this.api.query.balances.freeBalance(this.current_sender.address());
         }
 
         let receiver = await this.keyring.addFromUri(this.current_seed);
+        let previous = await this.api.query.balances.freeBalance(receiver.address());
         this.current_sender_nonce++;
         const tx_id = blake2AsHex(this.current_sender.address() + receiver.address() + this.current_sender_nonce).substr(2, 8);
         
         try {
-
+            // now useless
             let sender_balance = await this.api.query.balances.freeBalance(this.current_sender.address());
-
             let signed_tx = await this.api.tx.balances.transfer(receiver.address(), this.current_amount)
             	.sign(this.current_sender, { nonce: this.current_sender_nonce });
-        	
-			console.log('[DEBUG] TX: ' + tx_id + ', nonce: ' + this.current_sender_nonce + ', from: ' + this.current_sender_seed + '(balance: ' + sender_balance + '), to ' + this.current_seed + ", sending " + this.current_amount + ", GO");
-				
-			return await signed_tx.send();
+            let txHash = signed_tx
+                            .send()
+                .then((txHash: any) => { 
+                    //console.log('[DEBUG] TX: ' + tx_id + ', nonce: ' + this.current_sender_nonce + ', from: ' + this.current_sender_seed + '(balance: ' + sender_balance + '), to ' + this.current_seed + ", sending " + this.current_amount + ", txhash received: " + txHash);
+				});
 
-            	//.send((result: any) => {});
-				//.then((tx:any) => {
-				//	return tx;
-				//});
-	 	} catch(e) {
-        	console.log("[ERROR] " + e);
+			return this.api.query.balances.freeBalance(receiver.address(), (current: any) => {
+    					// Calculate the delta
+    					const change = current.sub(previous);
+						// Only display positive value changes (Since we are pulling `previous` above already,
+						// the initial balance change will also be zero)
+						if (!change.isZero()) {
+							previous = current;
+                            console.log('[DEBUG] TX: ' + tx_id + ', nonce: ' + this.current_sender_nonce + ', from: ' + this.current_sender_seed + '(balance: ' + sender_balance + '), to ' + this.current_seed + ", sending " + this.current_amount + ", txhash received: " + txHash + ", balance change: " + change );
+                    		return Promise.resolve(txHash);
+						}
+  					});
+        } catch(e) {
+        	console.log("[ERROR] BenchmarkStep: " + e);
         	throw e; // let caller know the promise was rejected with this reason
     	}
 		
